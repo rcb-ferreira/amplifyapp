1a5f5f51e05b0abffe65c810936668f8bb889aa8
rcb-ferreira <ferreira.rcb@gmail.com>
2019-06-08 07:17:09 +0200

    refactor(routing): add custom routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,52 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { TodoComponent } from './todos/component/todo.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'todos', component: TodoComponent },
-  {
-    path: '**',
-    redirectTo: '/todos',
-    pathMatch: 'full'
-  }
-];
+import { AppComponent } from './app.component';
+import { MuseumComponent } from './museum/museum.component';
+import { SettingsService } from './services/settings.service';
+import { ApiService } from './services/api.service';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UpdateDetailsRoutingModule } from './update-details-routing-module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+export function initSettings(settings: SettingsService) {
+  return () => settings.loadSettings();
+}
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    AppComponent,
+    MuseumComponent,
+    HomeComponent,
+    AboutComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    UpdateDetailsRoutingModule
+  ],
+  entryComponents: [
+    MuseumComponent
+  ],
+  providers: [
+    SettingsService,
+    ApiService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initSettings,
+      deps: [SettingsService],
+      multi: true,
+    }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+export class AppModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todos/component/todo.component';
-import { LoginComponent } from './auth/login/login.component';
+import { MuseumComponent } from './museum/museum.component';
+import { SettingsService } from './services/settings.service';
+import { ApiService } from './services/api.service';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UpdateDetailsRoutingModule } from './update-details-routing-module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+export function initSettings(settings: SettingsService) {
+  return () => settings.loadSettings();
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
-    LoginComponent,
+    MuseumComponent,
+    HomeComponent,
+    AboutComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    UpdateDetailsRoutingModule
   ],
-  providers: [],
+  entryComponents: [
+    MuseumComponent
+  ],
+  providers: [
+    SettingsService,
+    ApiService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initSettings,
+      deps: [SettingsService],
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
